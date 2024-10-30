ab3c478abfd4997f2e23fc7c6ebe4bb31c7e741d
ProkopenkoAV <arty1987@mail.ru>
2024-10-30 09:52:20 +0300

    init commit

---
+++ src/test/java/ru/netology/springbootapp/SpringbootappApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
